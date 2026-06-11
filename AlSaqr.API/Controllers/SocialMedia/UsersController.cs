e1b6e4a22f1b8e024dd1af074b3163c61c099212
AliA1997 <wgu.aalhaddad@gmail.com>
2026-06-10 20:30:57 -0400

    Remove some comments, and edit if statenent.

--- AlSaqr.API/Controllers/SocialMedia/UsersController.cs
+++ AlSaqr.API/Controllers/SocialMedia/UsersController.cs
@@ -50,7 +50,7 @@ namespace AlSaqr.API.Controllers.SocialMedia
         /// <summary>
         /// Update user based on user id.
         /// </summary>
-        /// <param name="data"></param>
+        /// <param name="request"></param>
         /// <returns></returns>
 
         [HttpPut]
@@ -84,7 +84,6 @@ namespace AlSaqr.API.Controllers.SocialMedia
         /// <summary>
         /// Delete user based on logged in user.
         /// </summary>
-        /// <param name="userId"></param>
         /// <returns></returns>
         [HttpDelete]
         public async Task<IActionResult> DeleteUser()
@@ -111,7 +110,6 @@ namespace AlSaqr.API.Controllers.SocialMedia
         /// <summary>
         /// Follow user endpoint
         /// </summary>
-        /// <param name="userId"></param>
         /// <param name="request"></param>
         /// <returns></returns>
         [HttpPatch("follow")]
@@ -146,7 +144,6 @@ namespace AlSaqr.API.Controllers.SocialMedia
         /// <summary>
         /// UnFollow user endpoint
         /// </summary>
-        /// <param name="userId"></param>
         /// <param name="request"></param>
         /// <returns></returns>
         [HttpPatch("unfollow")]
@@ -181,7 +178,6 @@ namespace AlSaqr.API.Controllers.SocialMedia
         /// <summary>
         /// Get posts to add whenever creating entity items such as communities, and lists.
         /// </summary>
-        /// <param name="userId"></param>
         /// <param name="currentPage"></param>
         /// <param name="itemsPerPage"></param>
         /// <param name="searchTerm"></param>
@@ -211,7 +207,6 @@ namespace AlSaqr.API.Controllers.SocialMedia
         /// <summary>
         /// Get users to add whenever creating entity items such as communities, and lists.
         /// </summary>
-        /// <param name="userId"></param>
         /// <param name="currentPage"></param>
         /// <param name="itemsPerPage"></param>
         /// <param name="searchTerm"></param>
@@ -228,7 +223,7 @@ namespace AlSaqr.API.Controllers.SocialMedia
                 return Unauthorized("To get users to add when upserting a record, you need to be logged in.");
             Guid.TryParse(loggedInUser.Id.ToString(), out var userId);
 
-            if (_socialMediaCacheService.CheckIfInitialUsersToAddCanBeRetrieved(currentPage, userId))
+            if (string.IsNullOrEmpty(searchTerm?.Trim()) && _socialMediaCacheService.CheckIfInitialUsersToAddCanBeRetrieved(currentPage, userId))
                 return Ok(_socialMediaCacheService.GetInitialUsersToAdd(userId));
 
             var result = await _userRepository.GetUsersToAdd(_supabase, userId, searchTerm, currentPage, itemsPerPage);
@@ -296,7 +291,7 @@ namespace AlSaqr.API.Controllers.SocialMedia
         /// Complete registration for a newly logged in user via social media provider.
         /// </summary>
         /// <param name="userId"></param>
-        /// <param name="data"></param>
+        /// <param name="request"></param>
         /// <returns></returns>
         [HttpPost("{userId}")]
         public async Task<IActionResult> CompleteRegistration(
